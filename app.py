68910644bb2d451cec5c95fc7287d4376774275a
Steve Goede <saggithub@gmail.com>
2015-07-01 20:49:33 +0200

    test Flask by examples

--- app.py
+++ app.py
@@ -14,6 +14,39 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "correct"
+
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
+
+
 # start the development server using the run() method
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
